299ae3ac58b69b03d1c160649e69864548454484
jesus <jesussanchezz0004@gmail.com>
2025-12-13 01:15:33 +0100

    TEST2: Se verifica la ordenación por número de cursos.

--- tdd-admisiones/src/test/java/app/domain/services/OrdenadorInscripcionesDomainServiceTest.java
+++ tdd-admisiones/src/test/java/app/domain/services/OrdenadorInscripcionesDomainServiceTest.java
@@ -39,9 +39,33 @@ class OrdenadorInscripcionesDomainServiceTest {
 	 
 	  assertEquals(ordenada, resultado, "ERROR: ordenar() debe ordenar las inscripciones por créditos si no hay empate.");
 	
-		
 	}
 	
+	
+	@Test
+	void test_GivenInscripcionesConMismoCredito_WhenOrdenar_ThenReturnListaOrdenadaPorCreditos() {
+		
+		IInscripcion inscripcionStub1 = mock(IInscripcion.class);
+		IInscripcion inscripcionStub2 = mock(IInscripcion.class);
+		IInscripcion inscripcionStub3 = mock(IInscripcion.class);
+		
+		when(inscripcionStub1.getCredito()).thenReturn(10.0);
+		when(inscripcionStub2.getCredito()).thenReturn(10.0);
+		when(inscripcionStub3.getCredito()).thenReturn(10.0);
+		
+		when(inscripcionStub1.getCursosEnTematica()).thenReturn(4);
+		when(inscripcionStub2.getCursosEnTematica()).thenReturn(5);
+		when(inscripcionStub3.getCursosEnTematica()).thenReturn(3);
+		
+	    List<IInscripcion> lista =List.of(inscripcionStub1, inscripcionStub2, inscripcionStub3);
+	    List<IInscripcion> ordenada = List.of(inscripcionStub2, inscripcionStub1, inscripcionStub3);
+	    
+	    
+		List<IInscripcion> resultado = ordenadorService.ordenar(lista);
+		
+		assertEquals(ordenada, resultado, "ERROR: ordenar() debe ordenar las inscripciones por número de cursos en la temática en caso de que haya empate en crédito");
+		
+	}
 	
 
 
